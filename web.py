8d40aca6be8b95e636e58d08e98021b9ee038992
gomesdasilva-thiago <55691713+gomesdasilva-thiago@users.noreply.github.com>
2021-09-18 13:58:08 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Olá Thiago Silva!"
+    return "Olá Thiago Silva! 02"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
